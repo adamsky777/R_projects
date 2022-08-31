26a4ca5cbaca6051da31e7c1a2b79a7e4ea89620
Adam Torkos <73426467+adamsky777@users.noreply.github.com>
2022-08-31 06:26:54 +0200

    Add files via upload

---
+++ Applied_Data_Analytics/Project_1_Lecture_1.R
@@ -0,0 +1,113 @@
+# Problem (A)
+
+
+
+X1 =3
+X2=-4
+
+Y1=2
+Y2=100
+
+#slope
+b = (Y1-Y2)/(X1-X2)
+
+# intercept
+a = ((Y2*X1) - (Y1*X2))  / (X1-X2)
+
+a
+b
+# correct
+# Problem (B)
+
+X1 = 0
+X2 = -11
+
+Y1 = -2
+Y2 = -100
+
+#slope
+b = (Y1-Y2)/(X1-X2)
+
+# intercept
+a = ((Y2*X1) - (Y1*X2))  / (X1-X2)
+
+a
+b
+
+# correct
+
+
+# Problem (C)
+
+random500= rnorm(500)
+hist((random500))
+summary(random500)
+
+# Problem (D)
+# Verify that the average of tildeX equals 0 (up to numerical precision).
+#Hint 1: Recall the command result <- X + 1 in intro.R.
+#It performs an operation (adding one) on each element of X, and places the result in the variable result.
+
+X = rnorm(20)
+X
+
+meanX = mean(X)
+
+tildeX = X - meanX
+tildeX
+mean(tildeX)
+# indeed tildeX is equals to zero
+
+mean(tildeX) < .0001
+
+
+
+#  Problem (E)
+X = rnorm(20)
+
+mean(X)
+sd(X)
+
+tildeX = (X - mean(X)) / (sd(X))
+
+
+mean(tildeX) < .0001
+# indeed smaller than .0001, we can conclude it is zero.
+
+
+# Problem  (F)
+
+X = rnorm(20)*10^10
+
+mean(X)
+sd(X)
+
+tildeX = (X - mean(X)) / (sd(X))
+
+
+mean(tildeX) < .0001
+# indeed smaller than .0001, even if we repeat it with 10¨^10 times, we can conclude it is zero.
+
+# Problem (G)
+
+
+n  = 15
+x = -0.5
+
+series = x^(0:n)
+
+1-x^(n+1)/(x-1)
+
+series
+sum(series)
+
+
+
+
+
+
+
+
+
+
+
